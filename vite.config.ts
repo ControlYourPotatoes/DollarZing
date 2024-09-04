7433fb5cb87b499e6f801fdd122531ed17ba6203
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-09-04 01:11:56 -0400

    Update brand image path in InfoCard and P2P Game Simulation Dashboard components

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,19 @@ export default defineConfig({
     },
   },
   base: '/DollarZing/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name ? assetInfo.name.split('.') : [];
+          let extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
 })
