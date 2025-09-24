25b61ec11d352a4adec02c03e8faa47e671c88a1
Alexander Puga <alexanderpuga78@gmail.com>
2025-09-24 21:48:05 +0000

    adjusting layout of nodes

--- src/features/financial-flow/layout/simpleLayout.ts
+++ src/features/financial-flow/layout/simpleLayout.ts
@@ -3,8 +3,8 @@ import {
   PresentationWorkflowNode,
 } from "@/shared/presentation";
 
-const GAP_Y = 120;
-const GAP_X = 260;
+const GAP_Y = 250;
+const GAP_X = 460;
 const MARGIN_X = 80;
 const MARGIN_Y = 60;
 
